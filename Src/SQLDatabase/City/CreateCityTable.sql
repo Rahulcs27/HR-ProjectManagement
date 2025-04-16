6441747662ad3df63909dba52f763957c3a68c6b
Rahul Suthar <90858008+Rahulcs27@users.noreply.github.com>
2025-04-16 16:29:09 +0530

    commit

---
+++ Src/SQLDatabase/City/CreateCityTable.sql
@@ -0,0 +1,26 @@
+CREATE TABLE City_ (
+    CityId INT IDENTITY(1,1) PRIMARY KEY,
+    
+    CityName NVARCHAR(100) NOT NULL,
+    
+    StateId INT NOT NULL,
+    CountryId INT NOT NULL,
+    
+    CityStatus BIT,
+    Action NVARCHAR(10),
+
+    WhoInserted NVARCHAR(50) NULL,
+    WhenInserted DATETIME NULL,
+
+    WhoUpdated NVARCHAR(50) NULL,
+    WhenUpdated DATETIME NULL,
+
+    WhoDeleted NVARCHAR(50) NULL,
+    WhenDeleted DATETIME NULL,
+
+    IsDeleted BIT DEFAULT 0,
+
+    -- Foreign Key Constraints
+    CONSTRAINT FK_City_State FOREIGN KEY (StateId) REFERENCES State_(StateId),
+    CONSTRAINT FK_City_Country FOREIGN KEY (CountryId) REFERENCES Country_(CountryId)
+);
