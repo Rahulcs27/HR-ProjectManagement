9289e239d0175358b3d333715260f9b620942c36
sumitsjogdand45 <sumitsj121@gmail.com>
2025-04-18 16:59:38 +0530

    login really works

--- Src/API/HR.API/obj/Debug/net8.0/HR.API.AssemblyInfo.cs
+++ Src/API/HR.API/obj/Debug/net8.0/HR.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c0c5e2de06e433b4430ca55434768ac5c81fd50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2149ef0dd6ee32a8881ac2bc15d59c38e714a1ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
